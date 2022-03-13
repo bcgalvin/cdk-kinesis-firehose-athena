eda95c3ee6a843136c2cd0775bd0a0e4d5c943cc
Bryan Galvin <bcgalvin@gmail.com>
2022-03-13 11:52:27 -0700

    Add ddb kinesis (#2)
    * add first pass at table + inds
    
    * deployment test green

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as path from 'path';
 import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, ITable, ProjectionType, Table, TableEncryption } from 'aws-cdk-lib/aws-dynamodb';
+import { IStream, Stream, StreamEncryption } from 'aws-cdk-lib/aws-kinesis';
 import { BlockPublicAccess, Bucket, BucketEncryption, IBucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 export class DynamoSeeder extends Construct {
   public readonly seedBucket: IBucket;
+  public readonly stream: IStream;
+  public readonly table: ITable;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -24,11 +28,49 @@ export class DynamoSeeder extends Construct {
       destinationKeyPrefix: 'data',
     });
 
+    const stream = new Stream(this, 'stream', {
+      streamName: `ddb-kinesis-stream`,
+      encryption: StreamEncryption.MANAGED,
+    });
+
+    const table = new Table(this, 'analytics-table', {
+      partitionKey: {
+        name: 'PK',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'SK',
+        type: AttributeType.STRING,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+      encryption: TableEncryption.AWS_MANAGED,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      kinesisStream: stream,
+    });
+    table.addGlobalSecondaryIndex({
+      indexName: 'gsi',
+      partitionKey: {
+        name: 'PK1',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'SK1',
+        type: AttributeType.STRING,
+      },
+      projectionType: ProjectionType.ALL,
+    });
+
     new CfnOutput(this, 's3-seed-bucket-output', {
       description: 'Seed s3 bucket name',
       value: seedBucket.bucketName,
     });
+    new CfnOutput(this, 'table-output', {
+      description: 'Dynamodb table name',
+      value: table.tableName,
+    });
 
     this.seedBucket = seedBucket;
+    this.stream = stream;
+    this.table = table;
   }
 }
--- test/seeder.unit.test.ts
+++ test/seeder.unit.test.ts
@@ -7,8 +7,12 @@ describe('DynamoSeeder Construct', () => {
 
   const { assert } = getSeederTestAssets(app);
 
-  test('construct should have an s3 bucket', () => {
+  test('construct should have a s3 bucket', () => {
     assert.resourceCountIs('AWS::S3::Bucket', 1);
+  });
+
+  test('construct should have a ddb table', () => {
+    assert.resourceCountIs('AWS::DynamoDB::Table', 1);
   });
 
   test('construct should have an s3 bucket deployment custom resource', () => {
@@ -23,6 +27,29 @@ describe('DynamoSeeder Construct', () => {
         IgnorePublicAcls: true,
         RestrictPublicBuckets: true,
       },
+    });
+  });
+
+  test('table has correct properties', () => {
+    assert.hasResourceProperties('AWS::DynamoDB::Table', {
+      GlobalSecondaryIndexes: [
+        {
+          IndexName: 'gsi',
+          KeySchema: [
+            {
+              AttributeName: 'PK1',
+              KeyType: 'HASH',
+            },
+            {
+              AttributeName: 'SK1',
+              KeyType: 'RANGE',
+            },
+          ],
+          Projection: {
+            ProjectionType: 'ALL',
+          },
+        },
+      ],
     });
   });
 });
