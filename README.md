202c31143466e5e87f92606d8439e1dfec0ec261
Bryan Galvin <bcgalvin@gmail.com>
2022-03-21 19:53:12 -0700

    add arch png

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # CDK Kinesis Firehose Athena
 
 ---
+Example cdk library demonstrating how to sync data originating from DynamoDB to a data warehouse using Kinesis Data
+Streams and Kinesis Data Firehose.
 
-### Sequence
+This solution leverages the fairly
+new [dynamic partitioning](https://docs.aws.amazon.com/firehose/latest/dev/dynamic-partitioning.html) feature from
+kinesis to continuously partition streaming data in Kinesis Data Firehose by using keys within data (this example
+uses `year`, `month`, `date` based on the createdAt field in the example dataset).
 
-![](./docs/images/sequence-diagram.png)
+### Architecture
 
+![](./docs/images/arch.png)
 ---
 
-#### DynamoDb
+#### Setup
 
-![](./docs/images/dynamo.png)
+![](./docs/images/setup.png)
+
+To simulate an active application, data is seeded to the DynamoDB table after the infrastructure stack deploys via
+the [s3 bucket deployment construct](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_s3_deployment.BucketDeployment.html)
+provided by the CDK.
+
+The example data generation script can be found under `assets/data` and a bit more information can be found
+under [docs/architecture/data ](https://github.com/bcgalvin/cdk-kinesis-firehose-athena/tree/main/docs/architecture/data)
 
 #### S3
 
--- assets/gen_seed_data.py
+++ assets/gen_seed_data.py
@@ -7,26 +7,12 @@ import numpy as np
 import pandas as pd
 from dynamodb_json import json_util
 
-ddb_client = boto3.client("dynamodb")
 s3_client = boto3.client("s3")
-ssm_client = boto3.client("ssm")
 
-SAGEMAKER_ROLE = "arn:aws:iam::570405429484:role/service-role/AmazonSageMaker-ExecutionRole-20201222T192023"
 SAGEMAKER_BUCKET = "aws-ml-blog-sagemaker-census-segmentation"
-DYNAMO_TABLE = ssm_client.get_parameter(Name="/test/ddb-table")["Parameter"]["Value"]
-PERIOD_START = pd.to_datetime("2022-01-01")
-PERIOD_END = pd.to_datetime("2022-03-14")
 
 
-def gen_random_dates(start: pd.Timestamp, end: pd.Timestamp, n=int):
-    start_u = start.value // 10**9
-    end_u = end.value // 10**9
-    return pd.to_datetime(np.random.randint(start_u, end_u, n), unit="s").strftime(
-        "%Y-%m-%d"
-    )
-
-
-def get_s3_file(bucket: str) -> None:
+def save_s3_file(bucket: str) -> None:
     obj_list = s3_client.list_objects(Bucket=bucket)
     file = [contents["Key"] for contents in obj_list["Contents"]]
     response = s3_client.get_object(Bucket=SAGEMAKER_BUCKET, Key=file[0])
@@ -34,31 +20,10 @@ def get_s3_file(bucket: str) -> None:
     data = pd.read_csv(
         io.BytesIO(response_body), header=0, delimiter=",", low_memory=False
     )
+    data["PK"] = "STATE#" + data["State"].astype(str)
+    data["SK"] = "COUNTY#" + data["County"].astype(str)
     data.to_csv("data/counties.csv", index=False)
 
 
-def get_s3_file(bucket: str) -> None:
-    obj_list = s3_client.list_objects(Bucket=bucket)
-    file = [contents["Key"] for contents in obj_list["Contents"]]
-    response = s3_client.get_object(Bucket=SAGEMAKER_BUCKET, Key=file[0])
-    response_body = response["Body"].read()
-    data = pd.read_csv(
-        io.BytesIO(response_body), header=0, delimiter=",", low_memory=False
-    )
-    data.dropna(inplace=True)
-    data["CreatedAt"] = gen_random_dates(PERIOD_START, PERIOD_END, data.shape[0])
-    return data
-
-
-def save_record(record: Dict, table: str) -> None:
-    ddb_client.put_item(TableName=table, Item=record)
-
-
 if __name__ == "__main__":
-    data = get_s3_file(SAGEMAKER_BUCKET)
-    data["PK"] = "STATE#" + data["State"].astype(str)
-    data["SK"] = "COUNTY#" + data["County"].astype(str)
-    ddb_data = [
-        json_util.dumps(row.to_dict(), as_dict=True) for index, row in data.iterrows()
-    ]
-    [ddb_client.put_item(TableName=DYNAMO_TABLE, Item=r) for r in ddb_data]
+    save_s3_file(SAGEMAKER_BUCKET)
